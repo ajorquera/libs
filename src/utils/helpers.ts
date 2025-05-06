167a8884d2b0f1dba917fc62756dfd0129d87f7d
andres <jorquera.ad@gmail.com>
2025-05-06 20:05:03 +0200

    Add checkEnvVars utility to validate required environment variables

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -54,4 +54,9 @@ export const mergeDeep = (target: unknowObj, ...sources: unknowObj[]): unknowObj
     return mergeDeep(target, ...sources) as unknowObj;
 }
 
-
+export const checkEnvVars = (envVars: string[]) => {
+    const missingEnvVars = envVars.filter((envVar) => !process.env[envVar]);
+    if (missingEnvVars.length > 0) {
+        throw new Error(`Missing environment variables: ${missingEnvVars.join(', ')}`);
+    }
+}
